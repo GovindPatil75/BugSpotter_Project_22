c35336ff06fd4c228093c97b61ba9b1ce9d43b04
Shubhangi Patil <shubhangi.deokar7@gmail.com>
2023-12-20 15:23:15 +0530

    Test005 added

---
+++ src/test/java/com/Test/Test_005.java
@@ -0,0 +1,13 @@
+package com.Test;
+
+public class Test_005 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("Test_005");
+		System.out.println("Test");
+
+	}
+
+}
